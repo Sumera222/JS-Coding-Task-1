c858b20b3ce151244a3082dda2ddc64b7260107b
SUMERA222 <83210529+Sumera222@users.noreply.github.com>
2021-06-24 23:39:55 +0500

    Add files via upload

---
+++ JS Coding Task 1/Topic 6-CheckDatatypes/app.js
@@ -0,0 +1,16 @@
+let num1 = 27; //integer value
+let num2 = 27.25;//decimal value
+let str = "string";// string value
+let num3 = true;//boolean value
+let num4 = null;//empty/null value
+
+//showing integer value to the variable
+console.log(typeof(num1));
+//showing decimal value to the variable
+console.log(typeof(num2));
+//showing string value to the variable
+console.log(typeof(str));
+//represents the boolean value to the variable
+console.log(typeof(num3));
+//represents the print version of null value to the variable
+console.log(typeof(num4));
